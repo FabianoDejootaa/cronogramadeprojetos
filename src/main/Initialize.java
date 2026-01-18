f5e06cb78a4d9bbda14589c51c643a200bf8175c
Fabiano Oliveira <fabianonotebook2003@gmail.com>
2026-01-17 23:04:35 -0300

    Inicialização do sistema criada, com monitoramento de desempenho. Implementação de métodos para inserir componentes e customiza-los na tela de login.

--- src/main/Initialize.java
+++ src/main/Initialize.java
@@ -1,5 +1,7 @@
 package main;
 
+import view.LoginView;
+
 /**
  * Classe de inicialização do sistema.
  * @author Fabiano Aparecido Mariano de Oliveira.
@@ -7,7 +9,29 @@ package main;
  * @version 0.0.1 (Em desenvolvimento).
  */
 public class Initialize {
+    /**
+     *  Método main.
+     *  @param args Argumentos do método main.
+     */
     public static void main(String args[]) {
         
+        new Thread(() -> {
+            final int MB = 1024 * 1024;
+            
+            short memoryUsage = (short)((Runtime.getRuntime().totalMemory()- 
+                Runtime.getRuntime().freeMemory()) / MB);
+            
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    System.out.println("Uso de memória: " + memoryUsage + " MB");
+                    Thread.sleep(1000);
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            LoginView view = new LoginView();
+        }).start();
     }
 }
--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,6 +1,11 @@
 package view;
 
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Classe de gerenciamento da tela de login.
@@ -10,15 +15,37 @@ import javax.swing.JFrame;
  */
 public class LoginView extends JFrame {
     
+    private final short WIDTH = 800;
+    private final short HEIGHT = 600;
+    
+    private JPanel background;
+    
     /**
-     *  Construtor da classe
-     *  @author Fabiano Aparecido Mariano de Oliveira.
-     *  @since 17/01/2026.
+     *  Construtor da classe.
      */
     public LoginView() {
         this.setTitle("CronoProject");
-        this.setSize(800, 600);
+        this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        customComponents();
+        insertComponents();
+        
+        this.setVisible(true);
+    }
+    /**
+     *  Método de customização de componentes da UI.
+     */
+    private void customComponents() {
+        background = new JPanel(new GridBagLayout());
+        background.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        background.setBackground(Color.LIGHT_GRAY);
+    }
+    /**
+     *  Método de inserção de componentes na UI.
+     */
+    private void insertComponents() {
+        this.add(background);
     }
 }
