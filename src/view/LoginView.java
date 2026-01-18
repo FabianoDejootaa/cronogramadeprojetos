ae585001dbb135e083d6cd20f86bd106a3a93243
Fabiano Oliveira <fabianonotebook2003@gmail.com>
2026-01-18 00:13:58 -0300

    Adicionando recursos de renderização gráfica.

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -3,9 +3,16 @@ package view;
 import java.awt.Color;
 import java.awt.GridBagLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Insets;
+import java.awt.GridBagConstraints;
+
+import java.net.URL;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 /**
  * Classe de gerenciamento da tela de login.
@@ -18,7 +25,19 @@ public class LoginView extends JFrame {
     private final short WIDTH = 800;
     private final short HEIGHT = 600;
     
+    private final Font CALIBRI_22 = new Font("Calibri", Font.BOLD, 22);
+    
+    private URL iconTitleUrl = LoginView.class.getResource("/imgs/icon_title.png");
+    
+    private ImageIcon iconTitle = new ImageIcon(iconTitleUrl);
+    
+    private GridBagConstraints gbc = new GridBagConstraints();
+    
     private JPanel background;
+    
+    private JLabel lbTitle;
+    
+    private ResolutionLabel lbIconTitle;
     
     /**
      *  Construtor da classe.
@@ -41,11 +60,40 @@ public class LoginView extends JFrame {
         background = new JPanel(new GridBagLayout());
         background.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         background.setBackground(Color.LIGHT_GRAY);
+        
+        lbTitle = new JLabel();
+        lbTitle.setFont(CALIBRI_22);
+        lbTitle.setForeground(Color.BLACK);
+        
+        lbIconTitle = new ResolutionLabel();
+        lbIconTitle.setIcon(iconTitle);
     }
     /**
      *  Método de inserção de componentes na UI.
      */
     private void insertComponents() {
         this.add(background);
+        
+        insertOnBackground();
+    }
+    private void insertOnBackground() {
+        restartGbc();
+        
+        background.add(lbIconTitle, gbc);
+        
+        gbc.gridx = 1;
+        
+        background.add(lbTitle, gbc);
+    }
+    private void restartGbc() {
+        gbc.gridheight = 1;
+        gbc.gridwidth = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.0;
+        gbc.weighty = 0.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0, 0, 0, 0);
     }
 }
---
+++ src/view/ResolutionLabel.java
@@ -0,0 +1,40 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JLabel;
+
+/**
+ *  Classe de reconstrução do JLabel.
+ *  Reconstrução da renderização de imagens com ativação de antiserrilhado.
+ *  @author Fabiano Aparecido Mariano de Oliveira.
+ *  @since 18/01/2026.
+ *  @version 0.0.1
+ */
+public class ResolutionLabel extends JLabel{
+    
+    /**
+     *  Construtor da classe.
+     */
+    public ResolutionLabel () {
+        super();
+    }
+    
+    /**
+     *  Método de renderização do objeto.
+     *  Sobreescrita para ativar a renderização com configuração bilinear da GPU.
+     *  @param g Objeto de manipulação de renderização.
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        
+        Graphics2D g2D = (Graphics2D) g;
+        
+        g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    
+        super.paintComponent(g);
+    }
+}
